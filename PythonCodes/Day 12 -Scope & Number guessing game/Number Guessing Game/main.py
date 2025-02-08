a62196f584480c5c837e8ee5ef91231dbab884d2
bbarczawork <bbarczawork@gmail.com>
2025-02-08 12:52:50 +0000

    day12

--- PythonCodes/Day 12 -Scope & Number guessing game/Number Guessing Game/main.py
+++ PythonCodes/Day 12 -Scope & Number guessing game/Number Guessing Game/main.py
@@ -1,3 +1,44 @@
 from art import logo
+import random
 
-print(logo)
+#print(logo)
+
+number = random.randint(1,100)
+#print(number)
+
+level = [10, 5]
+
+chooselevel = input("Choose a difficulty. Type 'easy' or 'hard': ").lower()
+gamelevel = 0
+if chooselevel == "easy":
+    gamelevel = level[0]
+elif chooselevel == "hard":
+    gamelevel = level[1]
+else:
+    print("wrong input")
+    print("your game lavel is easy")
+    gamelevel = level[0]
+#print(gamelevel)
+
+
+
+def checknumbers(number,gamelevel):
+    while gamelevel > 0:
+        userguessNumber = int(input("what is your guessing number: "))
+        print(userguessNumber)
+        if userguessNumber == number:
+            print("You found the number")
+            print("You WON")
+            gamelevel = 1
+        elif userguessNumber > number:
+            print("Your number is too big")
+        elif userguessNumber < number:
+            print("Your number is too small")
+        gamelevel -= 1
+        print(f"You have {gamelevel} live left ")
+        
+checknumbers(number,gamelevel)
+print(f"This was the number {number}")
+
+    
+
