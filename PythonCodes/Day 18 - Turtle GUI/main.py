52e82d01b86413b8c3fb8ff81f48a3df6df488b0
barczabalazs <barczabalazs@gmail.com>
2025-04-09 20:51:32 +0000

    RGB

--- PythonCodes/Day 18 - Turtle GUI/main.py
+++ PythonCodes/Day 18 - Turtle GUI/main.py
@@ -3,22 +3,41 @@ import random
 
 tim = t.Turtle()
 
-for _ in range(15):
-    tim.forward(10)
-    tim.penup()
-    tim.forward(10)
-    tim.pendown()
+#tim.forward(100)
+#tim.setheading(-90)
+#tim.heading()
+#tim.forward(100)
 
-
-
-colors  = ["red","green","blue","orange","purple","pink","yellow"] 
-num =4
-
-for _ in range(8):
-  degree = 360 / num
-  for _ in range(num):
-    tim.forward(100)
-    tim.right(degree)
-  num +=1
-  color = random.choice(colors) #Choose a random color
-  tim.color(color)
+def shape_maker(range_number):
+  colors  = ["red","green","blue","orange","purple","pink","yellow"] 
+  num =4
+  
+  
+  for _ in range(range_number):
+    degree = 360 / num
+    for _ in range(num):
+      tim.forward(100)
+      tim.right(degree)
+    num +=1
+    color = random.choice(colors) #Choose a random color
+    tim.color(color)
+  
+def change_color():
+  R = random.randint(0,255)
+  G = random.randint(0,255)
+  B = random.randint(0,255)
+  RGB = (R,G,B)
+  return (R,G,B)
+  
+def random_movment(run_movment):
+  direction = [0, 90, 180, 270]
+  tim.speed(10)
+  tim.pensize(10)
+  for _ in range(run_movment):
+    tim.color(change_color())
+    tim.forward(30)
+    tim.setheading(random.choice(direction))
+  
+  
+#shape_maker(8)
+random_movment(100)
