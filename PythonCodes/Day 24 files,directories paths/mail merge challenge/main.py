4476dd3fc84e057431f83d8f28135eef66f42f56
barczabalazs <barczabalazs@gmail.com>
2025-08-21 21:17:12 +0000

    name

--- PythonCodes/Day 24 files,directories paths/mail merge challenge/main.py
+++ PythonCodes/Day 24 files,directories paths/mail merge challenge/main.py
@@ -13,3 +13,30 @@ print("Mail merge program")
 
 with open("./PythonCodes/Day 24 files,directories paths/mail merge challenge/Input/Letters/starting_letter.txt" ,mode="r") as file:
     print(file.read())
+
+
+list = []
+temporary_file = ""
+
+with open("./PythonCodes/Day 24 files,directories paths/mail merge challenge/Input/Names/invited_names.txt" ,mode="r") as f:
+# with open("./Input/Names/invited_names.txt", mode="r") as f:
+    # print(f.readlines())
+    for name in f:
+        # print(name)
+        list.append(name)
+# print(list)
+
+with open("./PythonCodes/Day 24 files,directories paths/mail merge challenge/Input/Letters/starting_letter.txt" ,mode="r") as f:
+# with open("./Input/Letters/starting_letter.txt", mode="r") as f:
+    # print(f.read())
+    temporary_file = f.read()
+# print(temporary_file)
+
+for name in list:
+    # print(name)
+    new_file = temporary_file.replace("[name]", name)
+    file_name = name[:-1]
+    # print(file_name)
+    with open(f"./PythonCodes/Day 24 files,directories paths/mail merge challenge/Output/ReadyToSend/{file_name}.txt" ,mode="w") as new_letter:
+    # with open(f"./Output/ReadyToSend/{file_name}.txt", mode="w") as new_letter:
+        new_letter.write(new_file)
