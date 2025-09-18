bd0e776c6be209d16e6098e0e9777f622c4a96d0
barczabalazs <barczabalazs@gmail.com>
2025-09-18 19:22:01 +0000

    U.S. States Games

--- PythonCodes/Day 25 - CSV data/day-25-us-states-game/main.py
+++ PythonCodes/Day 25 - CSV data/day-25-us-states-game/main.py
@@ -1,46 +1,70 @@
-import turtle
-import pandas
 
+import pandas
+import turtle
 
 screen = turtle.Screen()
-screen.title("U.S. State Game")
+screen.title("U.S. States Games")
 image = "blank_states_img.gif"
 screen.addshape(image)
 turtle.shape(image)
 
+
 data = pandas.read_csv("50_states.csv")
 # print(data)
-
-i = 1
-while i < 51:
-    # print(i)
-    answer_state = screen.textinput(title=f"{i} / 50 Guess the State", prompt="What's another state's name")
-    print(answer_state)
-    if answer_state == "exit" or answer_state== "Exit":
-        quit()
-
-    for dat in data.state:
-        # print(dat)
-        if answer_state == dat:
-            print("good")
-            location_x = data[data["state"] == answer_state]
-            print(location_x)
-            location_y = data[data["state"] == answer_state]
-            print(location_y)
-    i+=1
+all_state = data.state.to_list()
 
 
-answer_state = screen.textinput(title="Guess the State", prompt="What's another state's name")
-print(answer_state)
-
-for dat in data.state:
-    # print(dat)
-    if answer_state == dat:
-        print("good")
+guessed_state = []
 
 
+while len(guessed_state) < 50:
+    answer_state = screen.textinput(title=f"{len(guessed_state)} / 50 Guess the State",
+                                    prompt="What`s another state`s name?").title()
+
+    if answer_state == "Exit":
+        # print(guessed_state)
+        missing_state = []
+
+        for state in all_state:
+            if state not in guessed_state:
+                missing_state.append(state)
+        print(missing_state)
+        new_date = pandas.DataFrame(missing_state)
+        new_date.to_csv("missing_data.csv")
+        break
+    if answer_state in all_state:
+        guessed_state.append(answer_state)
+        # print("got right")
+        state_data = data[data.state == answer_state]
+        t=turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+        t.goto(state_data.x.item() , state_data.y.item())
+        t.write(answer_state)
+    else:
+        print("not right")
 
 
 
+# def my_guess_game():
+#     i = 1
+#     while i < 51:
+#         print(i)
+#         answer_state = screen.textinput(title=f"{i} / 50 Guess the State", prompt="What`s another state`s name?")
+#         if answer_state == "exit":
+#             quit()
+#         if answer_state in all_state:
+#             print("got right")
+#             state_data = data[data.state == answer_state]
+#             t = turtle.Turtle()
+#             t.hideturtle()
+#             t.penup()
+#             t.goto(state_data.x.item(), state_data.y.item())
+#             t.write(state_data.state.item())
+#         else:
+#             print("not right")
+#
+#         i+=1
+#
+# my_guess_game()
 screen.exitonclick()
-
